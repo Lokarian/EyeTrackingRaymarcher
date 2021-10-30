fc34df6e3d2b05b87f34dd90abb331c33e8b1df4
Lokarian <samuelreithmeir@googlemail.com>
2021-10-30 02:26:28 +0200

    Introduced tree rendering

---
+++ Assets/second take/DataModels/DistanceTreeLinearModel.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceTreeLinearModel
+{
+    public int nodeType;
+    public Matrix4x4 translation;
+    public Vector3 color;
+    public Vector3 a;
+    public Vector3 b;
+}
